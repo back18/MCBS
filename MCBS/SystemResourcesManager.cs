15694ecfb67ee7e793c3400ea2af2920eab04941
Quan <2200191123@qq.com>
2023-09-24 23:05:45 +0800

    将 DefaultFont.bdf 嵌入程序集

--- MCBS/SystemResourcesManager.cs
+++ MCBS/SystemResourcesManager.cs
@@ -4,6 +4,7 @@ using QuanLib.BDF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -37,8 +38,12 @@ namespace MCBS
 
         public static void LoadAll()
         {
-            _DefaultFont = BdfFont.Load(Path.Combine(MCOS.MainDirectory.SystemResources.Fonts.DefaultFont));
-            LOGGER.Info($"默认字体文件加载完成，高度:{DefaultFont.Height} 半角宽度:{DefaultFont.HalfWidth} 全角宽度:{DefaultFont.FullWidth} 字符数量:{DefaultFont.Count}");
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            LOGGER.Info("开始加载默认字体资源文件");
+            using Stream defaultFontStream = assembly.GetManifestResourceStream("MCBS.SystemResource.DefaultFont.bdf") ?? throw new IndexOutOfRangeException();
+            _DefaultFont = BdfFont.Load(defaultFontStream);
+            LOGGER.Info($"完成，字体高度:{DefaultFont.Height} 半角宽度:{DefaultFont.HalfWidth} 全角宽度:{DefaultFont.FullWidth} 字符数量:{DefaultFont.Count}");
 
             _CursorManager = CursorManager.Load(MCOS.MainDirectory.SystemResources.Cursors.FullPath);
             LOGGER.Info($"光标文件加载完成");
