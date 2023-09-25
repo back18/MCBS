402ce9a2b635e049a16453ccd4e625192ba34be9
Quan <2200191123@qq.com>
2023-09-26 04:51:19 +0800

    修复了BMCLAPI下载源下载version.json无法解析json的bug

--- MCBS/MR.cs
+++ MCBS/MR.cs
@@ -101,8 +101,18 @@ namespace MCBS
             }
             else
             {
-                LOGGER.Info("正在下载: " + downloadProvider.VersionListUrl);
-                VersionList versionList = await VersionList.DownloadAsync(downloadProvider.VersionListUrl);
+                VersionList versionList;
+                try
+                {
+                    LOGGER.Info("正在下载: " + downloadProvider.VersionListUrl);
+                    versionList = await VersionList.DownloadAsync(downloadProvider.VersionListUrl);
+                }
+                catch (Exception ex)
+                {
+                    LOGGER.Warn("下载或解析失败，尝试切换到MOJANG官方下载源", ex);
+                    LOGGER.Info("正在下载: " + DownloadProvider.MOJANG_PROVIDER.VersionListUrl);
+                    versionList = await VersionList.DownloadAsync(DownloadProvider.MOJANG_PROVIDER.VersionListUrl);
+                }
 
                 if (!versionList.TryGetValue(MinecraftConfig.GameVersion, out var versionIndex))
                     throw new InvalidOperationException("未知的游戏版本：" + MinecraftConfig.GameVersion);
