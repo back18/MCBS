ab54980cfb83dc14e07a3dfa7d5fc7a343f27c8a
Quan <2200191123@qq.com>
2023-09-29 22:53:06 +0800

    添加 IHandleable 接口

---
+++ MCBS/IHandleable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBS
+{
+    public interface IHandleable
+    {
+        public void Handle();
+    }
+}
