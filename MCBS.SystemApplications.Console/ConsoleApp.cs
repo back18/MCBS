e9f74977deb3baf8768747054c442acbdd732334
Quan <2200191123@qq.com>
2023-11-07 22:50:02 +0800

    添加屏幕内控制台应用程序

---
+++ MCBS.SystemApplications.Console/ConsoleApp.cs
@@ -0,0 +1,26 @@
+﻿using MCBS.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBS.SystemApplications.Console
+{
+    public class ConsoleApp : IProgram
+    {
+        public const string ID = "Console";
+
+        public const string Name = "控制台";
+
+        public int Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
