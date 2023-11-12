572aa87ccbb510ccfc83e04c656fe5b3960c80c6
Quan <2200191123@qq.com>
2023-11-12 08:37:26 +0800

    添加二维码生成器应用程序，并调整了应用程序加载顺序

---
+++ MCBS.SystemApplications.QRCodeGenerator/QRCodeGeneratorApp.cs
@@ -0,0 +1,28 @@
+﻿using MCBS.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBS.SystemApplications.QRCodeGenerator
+{
+    public class QRCodeGeneratorApp : IProgram
+    {
+        public const string ID = "System.QRCodeGenerator";
+
+        public const string Name = "二维码生成器";
+
+        public int Main(string[] args)
+        {
+            this.RunForm(new QRCodeGeneratorForm());
+            return 0;
+        }
+
+        public void Exit()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ MCBS.SystemApplications.QRCodeGenerator/QRCodeGeneratorForm.cs
@@ -0,0 +1,13 @@
+﻿using MCBS.BlockForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBS.SystemApplications.QRCodeGenerator
+{
+    public class QRCodeGeneratorForm : WindowForm
+    {
+    }
+}
