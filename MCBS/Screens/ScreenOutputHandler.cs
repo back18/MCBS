07202ec2d2cf54abacd8b75fa842cbfb98710c94
Quan <2200191123@qq.com>
2023-12-07 23:30:58 +0800

    ScreenOutputHandler 类新增 CheckBlcok FillBlock 等方法

--- MCBS/Screens/ScreenOutputHandler.cs
+++ MCBS/Screens/ScreenOutputHandler.cs
@@ -1,5 +1,7 @@
 ﻿using MCBS.Rendering;
 using QuanLib.Minecraft;
+using QuanLib.Minecraft.Command;
+using QuanLib.Minecraft.Command.Senders;
 using SixLabors.ImageSharp;
 using System;
 using System.Collections.Concurrent;
@@ -16,18 +18,22 @@ namespace MCBS.Screens
     /// </summary>
     public class ScreenOutputHandler
     {
+        private const string AIR_BLOCK = "minecraft:air";
+        private const string LIGHT_BLOCK = "minecraft:light";
+
         public ScreenOutputHandler(Screen owner)
         {
             ArgumentNullException.ThrowIfNull(owner, nameof(owner));
 
             _owner = owner;
+            ScreenDefaultBlock = "minecraft:smooth_stone";
         }
 
         private readonly Screen _owner;
 
         public BlockFrame? LastFrame { get; internal set; }
 
-        public bool IsGenerated => LastFrame is not null;
+        public string ScreenDefaultBlock { get; set; }
 
         public void HandleOutput(BlockFrame frame)
         {
@@ -49,6 +55,47 @@ namespace MCBS.Screens
             {
                 await MCOS.Instance.MinecraftInstance.CommandSender.OnewaySender.SendOnewayBatchSetBlockAsync(arguments);
             }
+        }
+
+        public bool CheckBlcok(string blockId)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(blockId, nameof(blockId));
+
+            CommandSender sender = MCOS.Instance.MinecraftInstance.CommandSender;
+            for (int y = 0; y < _owner.Height; y++)
+                for (int x = 0; x < _owner.Width; x++)
+                {
+                    if (!sender.ConditionalBlock(_owner.ToWorldPosition(new(x, y)), blockId))
+                        return false;
+                }
+
+            return true;
+        }
+
+        public bool CheckAirBlock()
+        {
+            return CheckBlcok(AIR_BLOCK);
+        }
+
+        public bool FillBlock(string blockId, bool checkAirBlock = false)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(blockId, nameof(blockId));
+
+            if (checkAirBlock && !CheckAirBlock())
+                return false;
+
+            HandleOutput(new HashBlockFrame(_owner.Width, _owner.Height, blockId));
+            return true;
+        }
+
+        public bool FillDefaultBlock(bool checkAirBlock = false)
+        {
+            return FillBlock(ScreenDefaultBlock, checkAirBlock);
+        }
+
+        public bool FillAirBlock()
+        {
+            return FillBlock(AIR_BLOCK, false);
         }
 
         private IDictionary<Point, string> GetDifferencesPixels(BlockFrame frame)
