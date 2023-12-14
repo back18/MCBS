9de0c6561d5b296b7e5f5aa720813c8a5471586c
Quan <2200191123@qq.com>
2023-12-15 03:08:09 +0800

    添加 MCBS.SystemApplications.ScreenAdjuster 模块

---
+++ MCBS.SystemApplications.ScreenAdjuster/ScreenAdjusterApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCBS.Application;
+
+namespace MCBS.SystemApplications.ScreenAdjuster
+{
+    public class ScreenAdjusterApp : IProgram
+    {
+        public const string ID = "System.ScreenAdjuster";
+
+        public const string Name = "屏幕调节器";
+
+        public int Main(string[] args)
+        {
+            this.RunForm(new ScreenAdjusterForm());
+            return 0;
+        }
+
+        public void Exit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
