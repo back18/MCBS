d23f92425e7faeced9c8c41d443e11d424d2d0df
Quan <2200191123@qq.com>
2024-07-02 21:50:48 +0800

    添加 BlockFrameExtensions.ToImage 方法

--- MCBS/Drawing/Extensions/BlockFrameExtensions.cs
+++ MCBS/Drawing/Extensions/BlockFrameExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using MCBS.UI;
+using QuanLib.Game;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,28 @@ namespace MCBS.Drawing.Extensions
             ArgumentNullException.ThrowIfNull(blockFrame, nameof(blockFrame));
 
             return source.Overwrite(blockFrame, new(blockFrame.Width, blockFrame.Height), location, Point.Empty);
+        }
+
+        public static Image<Rgba32> ToImage(this BlockFrame source, Facing facing)
+        {
+            ArgumentNullException.ThrowIfNull(source, nameof(source));
+
+            Image<Rgba32> image = new(source.Width, source.Width, default);
+            for (int y = 0; y < source.Height; y++)
+                for (int x = 0; x < source.Width; x++)
+                {
+                    string blockId = source[x, y];
+
+                    if (string.IsNullOrEmpty(blockId))
+                        continue;
+
+                    if (!SR.Rgba32BlockMappings[facing].TryGetKey(blockId, out var rgba32))
+                        continue;
+
+                    image[x, y] = rgba32;
+                }
+
+            return image;
         }
     }
 }
