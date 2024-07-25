baeb321ad4f093772122ac7eb3b266b374e0e4d2
Quan <2200191123@qq.com>
2024-07-25 18:29:48 +0800

    添加 PathIconManager 类以及相关纹理文件

---
+++ MCBS.BlockForms/Utility/PathIconManager.cs
@@ -0,0 +1,46 @@
+﻿using MCBS.Config;
+using QuanLib.Core;
+using QuanLib.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBS.BlockForms.Utility
+{
+    public static class PathIconManager
+    {
+        public static Image<Rgba32> GetIcon(string path)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(path, nameof(path));
+
+            PathType pathType = FileUtil.GetPathType(path);
+
+            return pathType switch
+            {
+                PathType.Unknown => TextureManager.Instance["FileErrorIcon"],
+                PathType.Drive => TextureManager.Instance["DriverIcon"],
+                PathType.Directory => TextureManager.Instance["FolderIcon"],
+                PathType.File => GetFileIcon(path),
+                _ => throw new InvalidEnumArgumentException(),
+            };
+        }
+
+        public static Image<Rgba32> GetFileIcon(string path)
+        {
+            if (!File.Exists(path))
+                return TextureManager.Instance["FileErrorIcon"];
+
+            string extension = Path.GetExtension(path).TrimStart('.');
+            if (!ConfigManager.Registry.TryGetValue(extension, out var appId) ||
+                !MinecraftBlockScreen.Instance.AppComponents.TryGetValue(appId, out var applicationManifest))
+                return TextureManager.Instance["FileIcon"];
+
+            return applicationManifest.GetIcon();
+        }
+    }
+}
