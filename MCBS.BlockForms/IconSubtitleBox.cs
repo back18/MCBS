4ebffac049cc2f31e6dfcfbf1c996aa273c5d200
Quan <2200191123@qq.com>
2024-09-04 02:11:57 +0800

    添加 SubtitleBox 与 IconSubtitleBox 控件，用于显示滚动字幕

---
+++ MCBS.BlockForms/IconSubtitleBox.cs
@@ -0,0 +1,114 @@
+﻿using MCBS.BlockForms.Utility;
+using MCBS.Drawing;
+using MCBS.Events;
+using QuanLib.Core;
+using QuanLib.Core.Events;
+using QuanLib.Game;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBS.BlockForms
+{
+    public class IconSubtitleBox<TPixel> : ContainerControl<Control> where TPixel : unmanaged, IPixel<TPixel>
+    {
+        public IconSubtitleBox()
+        {
+            ClientSize = new(SR.DefaultFont.HalfWidth * 6, SR.DefaultFont.Height);
+
+            _Spacing = 0;
+
+            Icon_PictureBox = new();
+            Text_SubtitleBox = new();
+        }
+
+        public readonly PictureBox<TPixel> Icon_PictureBox;
+
+        public readonly SubtitleBox Text_SubtitleBox;
+
+        public int Spacing
+        {
+            get => _Spacing;
+            set
+            {
+                ThrowHelper.ArgumentOutOfMin(0, value, nameof(value));
+                if (_Spacing != value)
+                {
+                    _Spacing = value;
+                    RequestRedraw();
+                }
+            }
+        }
+        private int _Spacing;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            ChildControls.Add(Icon_PictureBox);
+            Icon_PictureBox.BorderWidth = 0;
+            Icon_PictureBox.AutoSize = false;
+            Icon_PictureBox.ClientSize = new(16, 16);
+            Icon_PictureBox.Skin.SetAllBackgroundColor(string.Empty);
+            Icon_PictureBox.Resize += Icon_PictureBox_Resize;
+
+            ChildControls.Add(Text_SubtitleBox);
+            Text_SubtitleBox.ClientSize = new(ClientSize.Width - 16 - Spacing * 3, 16);
+            Text_SubtitleBox.Stretch = Direction.Left;
+            Text_SubtitleBox.Resize += Text_SubtitleBox_Resize;
+
+            ActiveLayoutAll();
+        }
+
+        protected override void OnCursorEnter(Control sender, CursorEventArgs e)
+        {
+            base.OnCursorEnter(sender, e);
+
+            Text_SubtitleBox.Play();
+        }
+
+        protected override void OnCursorLeave(Control sender, CursorEventArgs e)
+        {
+            base.OnCursorLeave(sender, e);
+
+            Text_SubtitleBox.Pause();
+            Text_SubtitleBox.Reset();
+        }
+
+        public override void ActiveLayoutAll()
+        {
+            Icon_PictureBox.LayoutVerticalCentered(this, Spacing);
+            Text_SubtitleBox.LayoutVerticalCentered(this, Icon_PictureBox.RightLocation + 1);
+        }
+
+        private void Icon_PictureBox_Resize(Control sender, ValueChangedEventArgs<Size> e)
+        {
+            if (AutoSize)
+                AutoSetSize();
+        }
+
+        private void Text_SubtitleBox_Resize(Control sender, ValueChangedEventArgs<Size> e)
+        {
+            if (AutoSize)
+                AutoSetSize();
+        }
+
+        public override void AutoSetSize()
+        {
+            Size size = SR.DefaultFont.GetTotalSize(Text_SubtitleBox.Text);
+            size.Width += Icon_PictureBox.Texture.ImageSource.Width;
+            if (Icon_PictureBox.Texture.ImageSource.Height > size.Height)
+            {
+                size.Height = Math.Max(size.Height, Icon_PictureBox.Texture.ImageSource.Height);
+            }
+            size.Width += Spacing * 2;
+            size.Height += Spacing * 2;
+            ClientSize = size;
+            ActiveLayoutAll();
+        }
+    }
+}
---
+++ MCBS.BlockForms/SubtitleBox.cs
@@ -0,0 +1,156 @@
+﻿using MCBS.Drawing;
+using MCBS.Drawing.Extensions;
+using QuanLib.Core;
+using QuanLib.Core.Events;
+using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBS.BlockForms
+{
+    public class SubtitleBox : Control
+    {
+        public SubtitleBox()
+        {
+            BorderWidth = 0;
+            Skin.SetAllBackgroundColor(string.Empty);
+
+            IsPlaying = false;
+            IsLoopPlay = false;
+            _UpdateInterval = 1;
+            _PlayingSpeed = 1;
+            _PlayingPosition = 0;
+        }
+
+        private HashBlockFrame? _textFrame;
+
+        public bool IsPlaying { get; protected set; }
+
+        public bool IsLoopPlay { get; set; }
+
+        public int UpdateInterval
+        {
+            get => _UpdateInterval;
+            set
+            {
+                ThrowHelper.ArgumentOutOfMin(1, value, nameof(value));
+                _UpdateInterval = value;
+            }
+        }
+        private int _UpdateInterval;
+
+        public int PlayingSpeed
+        {
+            get => _PlayingSpeed;
+            set
+            {
+                ThrowHelper.ArgumentOutOfMin(0, value, nameof(value));
+                _PlayingSpeed = value;
+            }
+        }
+        private int _PlayingSpeed;
+
+        public int PlayingPosition
+        {
+            get => _PlayingPosition;
+            set
+            {
+                if (_PlayingPosition != value)
+                {
+                    _PlayingPosition = value;
+                    RequestRedraw();
+                }
+            }
+        }
+        private int _PlayingPosition;
+
+        protected override void OnTextChanged(Control sender, ValueChangedEventArgs<string> e)
+        {
+            base.OnTextChanged(sender, e);
+
+            _textFrame = null;
+            Reset();
+        }
+
+        protected override void OnAfterFrame(Control sender, EventArgs e)
+        {
+            base.OnAfterFrame(sender, e);
+
+
+            if (!IsPlaying || PlayingSpeed == 0)
+                return;
+
+            if (MinecraftBlockScreen.Instance.SystemTick % UpdateInterval != 0)
+                return;
+
+            Size textSize = SR.DefaultFont.GetTotalSize(Text);
+            int position = PlayingPosition;
+
+            if (IsLoopPlay)
+            {
+                int minPosition = -textSize.Width;
+                if (position <= minPosition)
+                    position = ClientSize.Width;
+                else
+                    position -= PlayingSpeed;
+            }
+            else
+            {
+                int minPosition = ClientSize.Width - textSize.Width;
+                if (minPosition >= 0)
+                    return;
+
+                if (position <= minPosition)
+                {
+                    position = minPosition;
+                    Pause();
+                }
+                else
+                {
+                    position -= PlayingSpeed;
+                }
+            }
+
+            PlayingPosition = position;
+        }
+
+        protected override BlockFrame Drawing()
+        {
+            BlockFrame baseFrame = base.Drawing();
+            if (string.IsNullOrEmpty(Text))
+                return baseFrame;
+
+            if (_textFrame is null)
+            {
+                _textFrame = new(SR.DefaultFont.GetTotalSize(Text));
+                int x = 0;
+                foreach (char c in Text)
+                {
+                    OverwriteContext overwriteContext = _textFrame.DrawBinary(SR.DefaultFont[c].GetBitArray(), GetForegroundColor().ToBlockId(), new(x, 0));
+                    x = overwriteContext.BaseEndPosition.X + 1;
+                }
+            }
+
+            baseFrame.Overwrite(_textFrame, new(PlayingPosition, 0));
+            return baseFrame;
+        }
+
+        public void Play()
+        {
+            IsPlaying = true;
+        }
+
+        public void Pause()
+        {
+            IsPlaying = false;
+        }
+
+        public void Reset()
+        {
+            PlayingPosition = 0;
+        }
+    }
+}
