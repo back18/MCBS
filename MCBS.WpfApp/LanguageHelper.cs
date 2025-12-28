346278f232c9fefe0165f789a7376a8e395f3dae
back18 <2200191123@qq.com>
2025-12-28 16:41:02 +0800

    支持主题与语言的用户设置及切换功能
    新增主题/语言设置持久化支持，包含 App.config、Settings、Resources 相关文件。实现 ThemeHelper 和 LanguageHelper，封装主题与语言切换逻辑，并在 MCBS.WpfApp.csproj 中配置资源和自动生成代码。为后续 UI 层调用和用户自定义体验提供基础。

---
+++ MCBS.WpfApp/LanguageHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+using WPFLocalizeExtension.Engine;
+
+namespace MCBS.WpfApp
+{
+    public static class LanguageHelper
+    {
+        public const string DEFAULT_LANGUAGE = "zh-CN";
+
+        private static Properties.Settings Settings => Properties.Settings.Default;
+
+        private static LocalizeDictionary LocalizeDictionary => LocalizeDictionary.Instance;
+
+        public static void Initialize()
+        {
+            string language = Settings.Language;
+            try
+            {
+                LocalizeDictionary.Culture = CultureInfo.GetCultureInfo(language);
+            }
+            catch
+            {
+                LocalizeDictionary.Culture = CultureInfo.GetCultureInfo(DEFAULT_LANGUAGE);
+            }
+
+            LocalizeDictionary.PropertyChanged += LocalizeDictionary_PropertyChanged;
+        }
+
+        public static CultureInfo Culture
+        {
+            get
+            {
+                return LocalizeDictionary.Culture;
+            }
+            set
+            {
+                if (!LocalizeDictionary.Culture.Equals(value))
+                    LocalizeDictionary.Culture = value;
+            }
+        }
+
+        private static void LocalizeDictionary_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(LocalizeDictionary.Culture))
+            {
+                Settings.Language = LocalizeDictionary.Culture.Name;
+                Settings.Save();
+            }
+        }
+    }
+}
---
+++ MCBS.WpfApp/ThemeHelper.cs
@@ -0,0 +1,44 @@
+﻿using iNKORE.UI.WPF.Modern;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCBS.WpfApp
+{
+    public class ThemeHelper
+    {
+        public const string THEME_LIGHT = nameof(ElementTheme.Light);
+        public const string THEME_DARK = nameof(ElementTheme.Dark);
+
+        private static Properties.Settings Settings => Properties.Settings.Default;
+
+        public static void Initialize()
+        {
+            ThemeManager.SetRequestedTheme(App.Current.MainWindow, AppTheme);
+        }
+
+        public static ElementTheme ParseTheme(string themeName)
+        {
+            return themeName switch
+            {
+                THEME_LIGHT => ElementTheme.Light,
+                THEME_DARK => ElementTheme.Dark,
+                _ => ElementTheme.Light,
+            };
+        }
+
+        public static ElementTheme AppTheme
+        {
+            get
+            {
+                return ParseTheme(Settings.AppTheme);
+            }
+            set
+            {
+                ThemeManager.SetRequestedTheme(App.Current.MainWindow, value);
+                Settings.AppTheme = value.ToString();
+                Settings.Save();
+            }
+        }
+    }
+}
