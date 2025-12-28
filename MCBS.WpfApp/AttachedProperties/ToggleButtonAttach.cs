9a82a32d437e703dc0c41a8ddab605add6b9b615
back18 <2200191123@qq.com>
2025-12-28 16:43:30 +0800

    引入自定义ToggleButton样式及自动折叠功能
    本次提交新增了两个自定义ToggleButton样式（含动画的主题切换按钮和常规开关样式），并实现了IsAutoFold附加属性以支持鼠标离开时的自动折叠动画。通过合并资源字典和附加属性，提升了界面交互的美观性与动效体验。

---
+++ MCBS.WpfApp/AttachedProperties/ToggleButtonAttach.cs
@@ -0,0 +1,78 @@
+﻿// 原始作者: SunnyDesignor
+// 来源: https://github.com/SunnyDesignor/WpfGorgeousThemeSwitch
+// 许可证: MIT
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace MCBS.WpfApp.AttachedProperties
+{
+    public static class ToggleButtonAttach
+    {
+        private const string MouseLeaveChecked = nameof(MouseLeaveChecked);
+        private const string MouseLeaveUnChecked = nameof(MouseLeaveUnChecked);
+
+        #region IsAutoFold
+        [AttachedPropertyBrowsableForType(typeof(ToggleButton))]
+        public static bool GetIsAutoFold(ToggleButton control)
+        {
+            return (bool)control.GetValue(IsAutoFoldProperty);
+        }
+
+        public static void SetIsAutoFold(ToggleButton control, bool value)
+        {
+            control.SetValue(IsAutoFoldProperty, value);
+        }
+
+        /// <summary>
+        /// 为具有 ToggleButtonGorgeousThemeSwitchStyle 样式的 <see cref="ToggleButton"/> 设置是否启用自动折叠
+        /// </summary>
+        public static readonly DependencyProperty IsAutoFoldProperty =
+            DependencyProperty.RegisterAttached(
+                "IsAutoFold",
+                typeof(bool),
+                typeof(ToggleButtonAttach),
+                new PropertyMetadata(false, ToggleButtonChanged));
+
+        private static void ToggleButtonChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            if (o is not ToggleButton control)
+                return;
+
+            if ((bool)e.NewValue)
+            {
+                control.MouseLeave += Control_MouseLeave;
+                control.Checked += Control_Checked;
+                control.Unchecked += Control_Checked;
+                if (!control.IsMouseOver)
+                    VisualStateManager.GoToState(control, control.IsChecked == true ? MouseLeaveChecked : MouseLeaveUnChecked, false);
+            }
+            else
+            {
+                control.MouseLeave -= Control_MouseLeave;
+                control.Checked -= Control_Checked;
+                control.Unchecked -= Control_Checked;
+                VisualStateManager.GoToState(control, "MouseOver", false);
+            }
+        }
+
+        private static void Control_Checked(object sender, RoutedEventArgs e)
+        {
+            var control = (ToggleButton)sender;
+            if (control.IsMouseOver)
+                return;
+
+            VisualStateManager.GoToState(control, control.IsChecked == true ? MouseLeaveChecked : MouseLeaveUnChecked, false);
+        }
+
+        private static void Control_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var control = (ToggleButton)sender;
+            VisualStateManager.GoToState(control, control.IsChecked == true ? MouseLeaveChecked : MouseLeaveUnChecked, false);
+        }
+        #endregion
+    }
+}
