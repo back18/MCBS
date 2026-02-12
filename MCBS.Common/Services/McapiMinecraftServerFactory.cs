a170c3d4e9e1cc8cb747ad94f5b538eedf1d018c
back18 <2200191123@qq.com>
2026-02-12 14:26:59 +0800

    新增 Minecraft 实例工厂及相关实现
    新增 IMinecraftInstanceFactory 接口和抽象基类 MinecraftInstanceFactoryBase，统一 Minecraft 实例的创建方式。实现了多种具体工厂类（如 Console、Hybrid、Rcon、Mcapi 等），根据配置灵活创建不同类型的 Minecraft 服务端或客户端实例，提升了系统的可扩展性和可维护性。所有工厂类均注入日志和配置服务，便于后续扩展和管理。

---
+++ MCBS.Common/Services/McapiMinecraftServerFactory.cs
@@ -0,0 +1,25 @@
+﻿using MCBS.Config.Minecraft;
+using MCBS.Services;
+using QuanLib.Core;
+using QuanLib.Minecraft.Instance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCBS.Common.Services
+{
+    public class McapiMinecraftServerFactory(ILoggerProvider loggerProvider, IMinecraftConfigProvider configProvider) : MinecraftInstanceFactoryBase(loggerProvider, configProvider)
+    {
+        public override MinecraftInstance CreateInstance()
+        {
+            MinecraftConfig config = _configProvider.Config;
+            return new McapiMinecraftServer(
+                config.MinecraftPath,
+                config.ServerAddress,
+                config.ServerPort,
+                config.McapiModeConfig.Port,
+                config.McapiModeConfig.Password,
+                _loggerProvider);
+        }
+    }
+}
